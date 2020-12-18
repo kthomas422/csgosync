7b1bace4d60feada2f0ca941a8564362f4cd8958
Kyle Thomas <mr.kyle.thomas@gmail.com>
2020-12-18 15:20:57 -0800

    smaller binaries

--- build-and-package.sh
+++ build-and-package.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+
+LINUX_ENV="env CGO_ENABLED=0 GOOS=linux GOARCH=amd64"
+WINTURDS_ENV="env CGO_ENABLED=0 GOOS=windows GOARCH=amd64"
+BUILD_CMD=`go build -ldflags '-s -w'`
+
 mkdir client server
 cp csgosync.yaml.example client/csgosync.yaml
 cp csgosyncd.yaml.example server/csgosyncd.yaml
 
-go build -o client/csgosync cmd/client/csgosync.go
-go build -o server/csgosyncd cmd/server/csgosyncd.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-s -w' -o client/csgosync cmd/client/csgosync.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-s -w' -o server/csgosyncd cmd/server/csgosyncd.go
 tar -czvf csgosync-linux-x64.tgz server client
 rm client/csgosync server/csgosyncd
 
-env GOOS=windows GOARCH=amd64 go build -o client/csgosync.exe cmd/client/csgosync.go
-env GOOS=windows GOARCH=amd64 go build -o server/csgosyncd.exd cmd/server/csgosyncd.go
+env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-s -w' -o client/csgosync.exe cmd/client/csgosync.go
+env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-s -w' -o server/csgosyncd.exe cmd/server/csgosyncd.go
 zip -r csgosync-windows-x64.zip server client
 rm -rf client server
 
